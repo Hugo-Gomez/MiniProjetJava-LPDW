a04c86265687286e05e5fcaea9528c94f165ba8e
Hugo Gomez <hugo.gomez9795@gmail.com>
2018-01-09 16:43:07 +0100

    Dev of the text area before game launch

--- src/fr/hugog/game/Panel.java
+++ src/fr/hugog/game/Panel.java
@@ -12,6 +12,11 @@ import javax.swing.JPanel;
 
 public class Panel extends JPanel {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void paintComponent(Graphics g){
 		
 		try {
--- src/fr/hugog/game/Window.java
+++ src/fr/hugog/game/Window.java
@@ -1,23 +1,40 @@
 package fr.hugog.game;
 import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Window extends JFrame {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Window() {
 		this.setTitle("Game");
 		this.setSize(800, 800);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-	    JPanel pan = new JPanel();
-	    pan.setBackground(Color.WHITE);        
-	    this.setContentPane(pan);
+				
+	    JPanel pan = new Panel();
+	    JTextField nameInput = new JTextField("");
+	    JButton startBtn = new JButton("Go !");
+	    Box nameStart = Box.createVerticalBox();
 	    
-	    this.setContentPane(new Panel());
+	    nameInput.setPreferredSize(new Dimension(200, 30));
+	    nameInput.setForeground(Color.black);
+	    
+	    nameStart.add(nameInput);
+	    nameStart.add(startBtn);
+	    
+	    pan.add(nameStart);
+	    
+	    this.setContentPane(pan);
 		
 		this.setVisible(true);
 	}
