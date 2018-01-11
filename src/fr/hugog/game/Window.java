77fdf04b67cc5cd1b7874790ccf1a2675d1e6e07
Hugo-Gomez <hugo.gomez9795@gmail.com>
2018-01-11 19:35:21 +0100

    Now the map is printed in the JPanel

--- src/fr/hugog/game/Window.java
+++ src/fr/hugog/game/Window.java
@@ -6,6 +6,8 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -15,7 +17,17 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 
+import fr.hugog.game.Element.Element;
+import fr.hugog.game.Element.Flower;
+import fr.hugog.game.Element.Gold;
+import fr.hugog.game.Element.Grass;
 import fr.hugog.game.Element.Hero;
+import fr.hugog.game.Element.Key;
+import fr.hugog.game.Element.Lock;
+import fr.hugog.game.Element.Monster;
+import fr.hugog.game.Element.Rock;
+import fr.hugog.game.Element.Trap;
+import fr.hugog.game.Element.Tree;
 import fr.hugog.game.map.Map;
 
 public class Window extends JFrame implements ActionListener {
@@ -28,12 +40,21 @@ public class Window extends JFrame implements ActionListener {
 	JPanel homePan = new HomePanel();
 	JPanel gamePan = new GamePanel();
 	JLabel inGameHeroName = new JLabel("");
+	JLabel inventory = new JLabel("");
+	JLabel space = new JLabel("<html><br/><html>");
     JTextField nameInput = new JTextField("");
     JButton startBtn = new JButton("Go !");
     String heroName;
+    
+    /**
+     * @see Element
+     */
+	Element[][] map;
 
 	public Window() 
 	{
+		ArrayList<Element> elements = Map.initializeElement();
+		this.map = initMap(elements);
 		this.setTitle("Game");
 		this.setSize(800, 800);
 		this.setLocationRelativeTo(null);
@@ -72,19 +93,81 @@ public class Window extends JFrame implements ActionListener {
         this.setContentPane(gamePan);
 		
 		gamePan.add(inGameHeroName);
+		gamePan.add(space);
 		
 		this.generateGameMap();
+		
+		gamePan.add(space);
+		
+		this.printObjects(hero);
 		
 		this.setVisible(true);
     }
 	
+	public Element[][] initMap(ArrayList<Element> element) {
+			
+		Element[][] map = new Element[20][20];
+		Random random = new Random();
+		int randomNumber;
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				if (i == 0 && j == 0) {
+					map[i][j] = new Hero();
+				} else {
+					randomNumber = random.nextInt(element.size());
+					map[i][j] = element.get(randomNumber);
+					element.remove(randomNumber);
+				}
+			}
+		}
+		return map;
+	}
+	
+	public static ArrayList<Element> initElement() {
+		ArrayList<Element> elements = new ArrayList<>();
+		for (int i = 0; i < 5 ; i++) {
+			elements.add(new Flower());
+		}
+		for (int i = 0; i < 5 ; i++) {
+			elements.add(new Tree());
+		}
+		for (int i = 0; i < 10 ; i++) {
+			elements.add(new Rock());
+		}
+		for (int i = 0; i < 3 ; i++) {
+			elements.add(new Key());
+		}
+		for (int i = 0; i < 3 ; i++) {
+			elements.add(new Lock());
+		}
+		for (int i = 0; i < 20 ; i++) {
+			elements.add(new Gold());
+		}
+		for (int i = 0; i < 10 ; i++) {
+			elements.add(new Trap());
+		}
+		for (int i = 0; i < 15 ; i++) {
+			elements.add(new Monster());
+		}
+		for (int i = 0; i < 329 ; i++) {
+			elements.add(new Grass(0, 0));
+		}
+		return elements;
+	}
+	
 	public void generateGameMap() {
 		for (int i = 0; i < 20; i++) {
 			for (int j = 0; j < 20; j++) {
-				JLabel l = new JLabel(i + " ", JLabel.CENTER);
+				JLabel l = new JLabel(this.map[i][j].getSymbol() + " ", JLabel.CENTER);
 	            gamePan.add(l);
 			}
 		}
+	}
+	
+	public void printObjects(Hero hero) {
+		inventory.setText("LifePoints : " + hero.getHealth() + " // Gold : " + hero.getGold() + " // Keys : " + hero.getKeys());
+		gamePan.add(inventory);
 	}
 	
 	public void actionPerformed(ActionEvent ae)
